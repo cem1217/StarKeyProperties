d5739a61e3b452843de5afed40b21442ba26a200
cem1217 <ce.martin1217@gmail.com>
2015-05-22 14:30:50 -0400

    First commit

---
+++ src/starkeyproperties/StarKeyProperties.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package starkeyproperties;
+
+/**
+ *
+ * @author Clara Ellie
+ */
+public class StarKeyProperties {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        // 
+    }
+    
+}
